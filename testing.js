1fdccc7b0e97b3d26eaccb12bc5620184f99fdb1
EliteDaMyth <elitemythyt@gmail.com>
2020-12-03 23:33:01 +0530

    Update testing.js

--- testing.js
+++ testing.js
@@ -9,10 +9,10 @@ var tokenizer = new Tokenizer();
 var parser = new Parser();
 var compiler = new Compiler();
 
-var tokens = tokenizer.tokenize(".-.-<>[.]`");
+var tokens = tokenizer.tokenize(input);
 var ast = parser.parse(tokens);
 var program = compiler.compile(ast);
-console.log(program)
+
 var vm = new VM(program);
 vm.run();
 console.log(vm.output_buffer)
